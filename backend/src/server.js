b5a057d6094596b564711c858094f07add0ac3ac
ishpreet95 <ishpreetsingh95@gmail.com>
2023-07-08 04:22:20 +0530

    session issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,12 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const maxAge = 30 * 24 * 60 * 60 * 1000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(
   session({
     secret: "your_secret_key",
@@ -21,29 +26,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser(function (user, done) {
-  done(null, user);
+  process.nextTick(function () {
+    return done(null, user);
+  });
 });
 
 passport.deserializeUser(function (user, done) {
-  done(null, user);
+  process.nextTick(function () {
+    // console.log(user);
+    return done(null, user);
+  });
 });
 
 const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 // app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.json({ message: "home page" });
-});
-app.get("/login", (req, res) => {
-  res.json({ message: "You are not logged in" });
-});
-app.get("/dashboard", (req, res) => {
-  res.json({ message: "You are logged in" });
-});
 
 app.get("/auth/google", strat.authenticate("google", { scope: ["profile"] }));
-let profile = "";
+// let profile = "";
 app.get(
   "/auth/google/callback",
   strat.authenticate("google", {
@@ -51,16 +52,13 @@ app.get(
     failureRedirect: "/login",
   }),
   (req, res) => {
-    // Save the profile data to the session
-    // req.session.profile = req.user; // Assuming 'profile' is the property that holds the profile data
-    // Redirect to the frontend route
-    profile = req.user;
     res.redirect("http://localhost:5173/");
   }
 );
-app.get("/api/profile", (req, res) => {
-  // const profile = req.session.profile; // Assuming you stored the profile data in the session
-  res.send(profile);
+app.get("/auth/user", (req, res) => {
+  console.log(req.session);
+  console.log(req.user);
+  res.send(req.user);
 });
 
 app.listen(port, () => console.log("server running on port " + port));
